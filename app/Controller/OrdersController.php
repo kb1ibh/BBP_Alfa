8dd9d13a51d6001733441fa5add948dff3a597b4
Michael Haynes <kb1ibh@gmail.com>
2014-08-28 13:09:20 -0400

    created Orders controller

---
+++ app/Controller/OrdersController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Orders Controller
+ *
+ * PHP v5
+ *
+ * @author     	Michael Haynes <kb1ibh@gmail.com>
+ * @copyright  	2014 Boston BioProducts Inc.
+ * @license    	MIT License
+ * @created		2014-08-28
+ * @modified	2014-08-28
+ */
+
+class OrdersController extends AppController {
+	
+}
