407f63d575f607cd0c2b28e473c7ec053eef6f50
Michael Haynes <kb1ibh@gmail.com>
2015-02-02 21:49:51 -0500

    added autocomplete find method to products model

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -16,11 +16,33 @@ App::uses('AppModel', 'Model');
 class Product extends AppModel {
 
 	public $name = 'Product';
+	public $displayField = 'description';
 	
 	public $hasmany = array(
 		'Detail' => array(
 			'className' => 'Detail'
 		)
 	);
+	
+	
+	/*
+	 *  Autocomplete find method
+	 *  Returns product data formatted for jQuery UI Autocomplete 
+	 */
+	public $findMethods = array('autocomplete' => true);
+	protected function _findAutoComplete($state, $query, $results = array()) {
+		if ($state === 'before') {
+			return $query;
+		} elseif($state === 'after') {
+			$data = array();
+			foreach($results as $result) {
+				$result[$this->alias]['label'] = $result[$this->alias]['aa_cat'] . ' - ' . $result[$this->alias]['bbp_cat'] . ' - ' . $result[$this->alias]['vol'];
+				$result[$this->alias]['desc'] = $result[$this->alias]['description'];
+				array_push($data, $result[$this->alias]);
+			}
+			
+			return $data;
+		}
+    }
 
 }
